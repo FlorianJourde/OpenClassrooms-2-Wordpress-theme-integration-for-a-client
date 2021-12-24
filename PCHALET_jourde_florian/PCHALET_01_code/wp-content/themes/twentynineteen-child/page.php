396372943d2b61c74d1fb984f18f5b9cd5ad91e4
Florian Jourde <jourdeflorian@gmail.com>
2021-12-24 17:19:11 +0100

    Updating work

---
+++ PCHALET_jourde_florian/PCHALET_01_code/wp-content/themes/twentynineteen-child/page.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+      <header class="banner" style="background-image: url('<?php bloginfo('template_directory'); ?>-child/images/chalet_2.jpg');">
+        <div class="blog-info">
+          <div class="wrapper">
+            <h1><?= get_bloginfo('name')?></h1>
+            <h2><?= get_bloginfo( 'description')?></h2>
+          </div>
+        </div>
+      </header>
+
+      <?php
+
+      $query = new WP_Query (
+        array(
+          'post_type' => 'banner',
+          'post_per_page' => -1,
+        )
+      );
+
+      while ( $query->have_posts() ) : $query->the_post(); ?>
+
+        <!--        <img class="thumbnail" src="--><?//= get_field('banner')['url']; ?><!--" alt="">-->
+        <!--        --><?php //var_dump(get_field('banner')); ?>
+
+      <?php
+
+      endwhile;
+
+      //      wp_reset_postdata();
+
+      ?>
+
+      <section class="rentals">
+        <div class="wrapper">
+          <h2>Locations</h2>
+          <div class="cards">
+
+            <?php
+
+            $query = new WP_Query (
+              array(
+                'post_type' => 'location',
+                'posts_per_page' => 4,
+              )
+            );
+
+            while ( $query->have_posts() ) : $query->the_post(); ?>
+
+              <div class="card">
+                <img class="thumbnail" src="<?= get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+                <div class="content">
+                  <h3 class="title"><?php the_title(); ?></h3>
+                  <p class="description"><?php the_field('description', get_the_ID()); ?></p>
+                  <div class="infos">
+                    <p class="price"><?php the_field('prix_a_la_semaine', get_the_ID()); ?> €</p>
+                    <p class="capacity"><?php the_field('capacite', get_the_ID()); ?> places</p>
+                  </div>
+                </div>
+              </div>
+
+            <?php
+
+            endwhile;
+
+            wp_reset_postdata();
+
+            ?>
+
+          </div>
+        </div>
+      </section>
+      <section class="sales">
+        <div class="wrapper">
+          <h2>Ventes</h2>
+          <div class="cards">
+
+            <?php
+
+            $query = new WP_Query (
+              array(
+                'post_type' => 'vente',
+                'posts_per_page' => 4,
+              )
+            );
+
+            while ( $query->have_posts() ) : $query->the_post(); ?>
+
+              <div class="card">
+                <img class="thumbnail" src="<?= get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+                <div class="content">
+                  <h3 class="title"><?php the_title(); ?></h3>
+                  <p class="description"><?php the_field('description', get_the_ID()); ?></p>
+                  <div class="infos">
+                    <p class="price"><?php the_field('prix', get_the_ID()); ?> €</p>
+                    <p class="surface"><?php the_field('superficie', get_the_ID()); ?> m²</p>
+                  </div>
+                </div>
+              </div>
+
+            <?php
+
+            endwhile;
+
+            wp_reset_postdata();
+
+            ?>
+
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+
+<?php
+get_footer();
